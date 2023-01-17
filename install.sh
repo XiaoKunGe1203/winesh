fc8afb11c8a4e0cad521b93466325552d124c5a9
XiaoKunGe1203 <119796222+XiaoKunGe1203@users.noreply.github.com>
2023-01-18 01:06:02 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Install wine
+codename=$(lsb_release -c --short)
+dpkg --add-architecture i386
+mkdir -pm755 /etc/apt/keyrings
+wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/${codename}/winehq-${codename}.sources
